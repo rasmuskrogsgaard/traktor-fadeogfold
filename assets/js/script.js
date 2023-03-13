c127549c7a9835e52754416a5635617d913ded6d
Rasmus Krogsgaard <rasmus.krogsgaard@gmail.com>
2023-03-13 12:58:53 +0100

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 let fadingImg = document.getElementsByClassName('tractorimg');
 
 window.addEventListener('load', () => {
-    
+    console.log('loaded');
+for (const myElement of fadingImg) {
+    myElement.classList.toggle('showImg');
 }
+});
