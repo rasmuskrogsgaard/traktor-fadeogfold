378008a28453c170fa6a6910641d5ca9244d09a5
Eon4 <ladyforest85@gmail.com>
2023-03-16 13:03:34 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,38 +15,7 @@ for (const myElement of fadingImg) {
 //FORM 
 
 
-// let mySubmit=document.getElementsByClassName('email');
-// let myButton=document.getElementById('myButton');
 
-// myButton.addEventListener('click',()=>{
-//     for(const email of mySubmit)
-//     email.classList.toggle('animateShake');
-//     });
-
-//SIMPLE VALIDATION 
-
-// const validate = (form) => {
-//     console.log("dette er fra vores form ", form);
-  
-//     let errors = [];
-  
-//     console.log(form.email.value);
-  
-//     if (form.email.value === "") {
-//       errors.push("Email er påkrævet\n");
-//     }
-//     if (errors.length > 0) {
-//       console.log("Number of errors are ", errors.length);
-//       let msg = "Følgende fejl er fundet:: \n\n";
-//       for (let i = 0; i < errors.length; i++) {
-//         msg += errors[i];
-//       }
-//       alert(msg);
-//       return false;
-//     }
-  
-//     return true;
-//   };
 
 // foldOut
 
